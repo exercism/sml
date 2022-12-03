55fbf1169dbb23b25f740800a8dc6f542510be94
Reinhard Stahn <reinhardstahn@t-online.de>
2022-12-03 19:06:42 +0100

    Validate template solution (heuristically)

--- scripts/test-in-docker.sh
+++ scripts/test-in-docker.sh
@@ -7,19 +7,38 @@ all_slugs="$*"
 exit_code=0
 
 for slug in $all_slugs; do
+  local_exit_code=0
+
   rm -rf /solution
   cp -r /exercises/practice/${slug} /solution
+
+  # Check the template file for static errors and type inconsistencies with test.sml
+  bin/run.sh ${slug} /solution /solution > /dev/null
+  mv /solution/results.json /solution/results-template.json
+  template_errors=$(jq '.message' /solution/results-template.json | grep "Static Errors")
+
+  # Check if the example solution passes
   cp /solution/.meta/example.sml /solution/${slug}.sml
-  bin/run.sh ${slug} /solution /solution
+  bin/run.sh ${slug} /solution /solution > /dev/null
+  pass_pattern=$(cat /solution/results.json | jq -r tostring | grep "{\"version\":1,\"status\":\"pass\"")
 
-  results=$(cat /solution/results.json)
-  selection=$(echo "$results" | jq -r tostring | grep "{\"version\":1,\"status\":\"pass\"")
+  errors=""
 
-  if [ -z $selection ]; then
-    echo -e "${slug}: \e[31mFAILED\e[0m\n\n$(echo "$results" | jq -r '.message')\n\n"
-    exit_code=1;
+  if [ -n "$template_errors" ]; then
+    errors="Template is faulty:\n$(cat /solution/results-template.json | jq -r '.message')"
+    local_exit_code=1;
+  fi
+
+  if [ -z "$pass_pattern" ]; then
+    errors="${errors}\n\nSolution is incorrect:\n$(cat /solution/results.json | jq -r '.message')"
+    local_exit_code=1;
+  fi
+
+  if [ $local_exit_code = 0 ]; then
+    echo -e "${slug}: \e[32mPASSED\e[0m"
   else
-    printf "${slug}: \e[32mPASSED\e[0m\n\n";
+    exit_code=1
+    echo -e "${slug}: \e[31mFAILED\e[0m\n\n${errors}\n\n"
   fi
 done
 
